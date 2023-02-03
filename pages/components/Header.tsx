a587dd666c0778a5109c16dbb06cb847e32cd8eb
takaaki tabei <43052438+tabeaki@users.noreply.github.com>
2023-02-04 08:35:50 +0900

    update

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
                     <div className="lg:justify-center flex">
                     </div>
                     <div className="justify-end px-2 flex">
-                        <div className="bg-[url('/opensea_icon.png')] invisible md:visible px-2 pt-8 mx-1 bg-center bg-cover"><a className="px-2 py-4" href=""></a></div>
+                        <div className="bg-[url('/opensea_icon.png')] invisible md:visible px-2 pt-8 mx-1 bg-center bg-cover"><a className="px-2 py-4" href="https://opensea.io/ja/collection/cnpes-generative"></a></div>
                         <div className="bg-[url('/twitter_icon.png')] invisible md:visible px-2 pt-8 mx-1 bg-center bg-cover"><a className="px-2 py-4" href="https://twitter.com/CNPEnjoySports"></a></div>
                     </div>
                 </div>
